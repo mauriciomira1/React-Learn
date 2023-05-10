2cb496e21ef3e74e81c854d39296fed6fd008285
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-05-10 19:17:40 -0300

    bootstrap config

--- missacifras/src/App.tsx
+++ missacifras/src/App.tsx
@@ -1,1 +1,5 @@
 import Menu from "./components/generic/menu";
+import '../node_modules/boot'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
+<script src="./node_modules/bootstrap/dist/js/bootstrap.js" defer></script>
