163251aa48a68674ad8f7ca0e1bea21cd3ba444c
Maurício Miranda Gomes <121832237+mauriciomira1@users.noreply.github.com>
2023-05-11 17:48:38 -0300

    continue menu

--- missacifras/src/App.tsx
+++ missacifras/src/App.tsx
@@ -2,9 +2,10 @@ import Menu from "./components/generic/menu";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import logo from "./assets/logo/logo(305x91).png";
+import userLogo from "./assets/generic/userLogo.png";
 
 export default function App() {
-  return <Menu logo={logo}></Menu>;
+  return <Menu logo={logo} userLogo={userLogo}></Menu>;
 }
 
 {
--- missacifras/src/components/generic/menu/index.tsx
+++ missacifras/src/components/generic/menu/index.tsx
@@ -20,11 +20,11 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 export default function Menu(props: any) {
   return (
     <>
-      {["md"].map((expand) => (
+      {["lg"].map((expand) => (
         <Navbar key={expand} bg="light" expand={expand} className="mb-3">
           <Container fluid>
             <Navbar.Brand href="#">
-              <img src={props.logo} alt="Logotipo" />
+              <img src={props.logo} id="logo" alt="Logotipo" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
@@ -38,11 +38,22 @@ export default function Menu(props: any) {
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <Nav.Link href="#action1">Acordes</Nav.Link>
+                  <Nav.Link href="#action1">Afinador</Nav.Link>
+                  <Nav.Link href="#action2">Fale Conosco</Nav.Link>
+                  <Nav.Link href="#action2">Colabore</Nav.Link>
                   <NavDropdown
-                    title="Dropdown"
+                    title={<img src={props.userLogo} id="userLogo"></img>}
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
                     <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
@@ -55,15 +66,6 @@ export default function Menu(props: any) {
                     </NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
