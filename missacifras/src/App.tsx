82f83d45db6e0c924d6c22ecd7974a568973e1d5
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-05-13 08:35:33 -0300

    desinstalando bootstrap e refazendo menu

--- missacifras/src/App.tsx
+++ missacifras/src/App.tsx
@@ -1,11 +1,12 @@
-import Menu from "./components/generic/menu";
+import Menu from "./components/generic/Menu";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import logo from "./assets/logo/logo(305x91).png";
 import userLogo from "./assets/generic/userLogo.png";
+import searchLogo from "./assets/generic/lupa.png";
 
 export default function App() {
-  return <Menu logo={logo} userLogo={userLogo}></Menu>;
+  return <Menu logo={logo} userLogo={userLogo} searchLogo={searchLogo}></Menu>;
 }
 
 {
