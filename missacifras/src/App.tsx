2a0519cd708815cb4b2057369798afbf318a0f37
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-05-14 18:21:39 -0300

    aprendendo aula07

--- missacifras/src/App.tsx
+++ missacifras/src/App.tsx
@@ -1,8 +1,3 @@
-import Menu from "./components/generic/Menu";
-import logo from "./assets/logo/logo(305x91).png";
-import userLogo from "./assets/generic/userLogo.png";
-import searchLogo from "./assets/generic/lupa.png";
-
 export default function App() {
-  return <Menu logo={logo} userLogo={userLogo} searchLogo={searchLogo}></Menu>;
+  return <></>;
 }
