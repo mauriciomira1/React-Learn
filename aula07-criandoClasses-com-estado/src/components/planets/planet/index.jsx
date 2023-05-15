75e2d30ace8b97de9e721681807f73f1a8c908b9
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-05-14 23:54:56 -0300

    continuando aulas

---
+++ aula07-criandoClasses-com-estado/src/components/planets/planet/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Planet = () => {
+  return (
+    <>
+      <h4>Mercúrio</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laborum
+        perferendis, ut et fuga eaque minima sit voluptatem est quas delectus
+        libero suscipit deserunt illo error reprehenderit! Fuga, porro
+        doloremque.
+      </p>
+    </>
+  );
+};
+
+export default Planet;
