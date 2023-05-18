80dbcd1d7bd4a35dc49f7daadb241d2401889311
Maurício Miranda Gomes <121832237+mauriciomira1@users.noreply.github.com>
2023-05-18 17:33:16 -0300

    continuando react youtube

---
+++ aula08-continuando_YT/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+// 1 - configurando router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//3 - criei, agora importo as páginas
+import Home from "./routes/Home.jsx";
+import Contact from "./routes/Contact.jsx";
+
+//2 - criar constante
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
