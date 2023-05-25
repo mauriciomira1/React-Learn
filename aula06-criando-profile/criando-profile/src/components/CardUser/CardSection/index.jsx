fa009dbae5e9434359a06a0e56450f3d4df9fabd
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-05-25 07:32:25 -0300

    contionuando react hooks

--- aula06-criando-profile/criando-profile/src/components/CardUser/CardSection/index.jsx
+++ aula06-criando-profile/criando-profile/src/components/CardUser/CardSection/index.jsx
@@ -1,7 +1,11 @@
 import styles from "./styles.module.css";
 
 const CardSection = (props) => {
-  return <div className={styles.wrapper}>{props.children}</div>;
+  return (
+    <div {...props} className={`${styles.wrapper} ${props.className}`}>
+      {props.children}
+    </div>
+  );
 };
 
 export default CardSection;
--- aula06-criando-profile/criando-profile/src/components/CardUser/index.jsx
+++ aula06-criando-profile/criando-profile/src/components/CardUser/index.jsx
@@ -1,33 +1,40 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Title from "../Title";
 import CardSection from "./CardSection";
 import Social from "./Social";
+import styles from "./styles.module.css";
 
 export default function CardUser(props) {
+  const [followButtonText, setFollowButtonText] = useState("Follow");
+  function handleClick() {
+    /* alert("Agora você está seguindo!"); */
+    setFollowButtonText("Following");
+  }
+
   return (
     <>
       <img className="photo" src={props.photoUser} alt="User" />
       <Title>
         <span>{props.name}</span>
-        <button>
-          <a
-            href="https://www.instagram.com/mauriciomira1/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            Seguir
-          </a>
+        <button className={styles.followButton} onClick={handleClick}>
+          {" "}
+          {followButtonText}
         </button>
       </Title>
       <CardSection> {props.hardSkills}</CardSection>
       <CardSection>{props.phoneNumber}</CardSection>
       <CardSection>{props.email}</CardSection>
-
-      <div className="social">
+      <CardSection
+        className="social"
+        id="links-section"
+        data-test="some value"
+        aria-label="social-links"
+      >
         <Social link={props.gitHub}>GitHub</Social>
         <Social link={props.linkedIn}>LinkedIn</Social>
         <Social link={props.instagram}>Instagram</Social>
-      </div>
+      </CardSection>
     </>
   );
 }
