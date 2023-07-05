1187f65b80ea42cc6f81c52d4e8b8407049a4e77
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-07-04 21:47:03 -0300

    iniciando site Viagens do YT

---
+++ site-viagens/src/app/about/components/Trips.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Trips = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <h1>Trips</h1>
+    </div>
+  );
+};
+
+export default Trips;
